f5c094e30cd57fb74de4954e8f481bec93daa361
Raiyaxi-ZIaoi <82474669+Raiyaxi-Ziaoi@users.noreply.github.com>
2022-08-17 23:35:54 +0100

    Code added

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
+<img src="https://i.imgur.com/lRzzpEU.png"></img>
+
 # ALSABAH
-An assembly-ish thing intended to be a compile target for future versions of ALNOOT
+
+<div align="center"><h2><br/><br/>
+ An assembly-ish thing intended to be a compile target for future versions of ALNOOR<br/><br/>Made by Raiyaxi Ziaoi
+</h2></div>
+<br>
+
+Keywords: <br/>
+PSH: Pushes a value to the stack; "PSH 3"<br/>
+ARG: Adds a register with default value '0'; "ARG"<br/>
+PLL: Pulls a value from a register and pushes it to the stack; "PLL 0"<br/>
+SET: Sets a register to a value from the stack; "SET 0 -1"<br/>
+MOV: Copies a value from one register to another and resets the first; "MOV 0 1"<br/>
+HLD: Makes a register hold a value specified; "HLD 0 1"<br/>
+POP: Deletes last value in the stack; "POP"<br/>
+ADD, SUB, MUL, DIV: Does arithmetic to the last two values of the stack and deletes them and pushes the result<br/>
+LOG: Logs value of member of stack given an indice; "LOG - 1"<br/>
+HLT: Halts the program; "HLT"<br/>
+PSS: Starts a one-line comment; "PSS 🗿"<br/>
+CHP: Prints a character given an indice of the value in question on the stack<br/>
+RGP: Prints a character given the register number<br/>
+MRG: Adds multiple registers; "MRG 10"<br/>
---
+++ main.py
@@ -0,0 +1,74 @@
+
+def main():
+    text = []
+    stack = []
+    registers = []
+    sp = 0
+    ip = 0
+    print("Please enter a filename: ")
+    with open(input(), "r") as f:
+        text = f.readlines()
+    while True:
+        token = text[ip].split(' ')
+        ftoken = token[0].replace('\n', '')
+        match ftoken:
+            case "PSH":
+                stack.append(token[1])
+            case "ARG":
+                registers.append(0)
+            case "MRG":
+                for x in range((int)(token[1])):
+                    registers.append(0)
+            case "PLL":
+                stack.append(registers[(int)(token[1])])
+            case "SET":
+                registers[(int)(token[1])] = stack[(int)(token[2])]
+            case "MOV":
+                registers[(int)(token[1])] = registers[(int)(token[2])]
+                registers[(int)(token[1])] = 0
+            case "HLD":
+                registers[(int)(token[1])] = token[2]
+            case "POP":
+                stack.pop(-1)
+            case "ADD":
+                result = (int)(stack[-1]) + (int)(stack[-2])
+                stack.pop(-1)
+                stack.pop(-1)
+                stack.append(result)
+            case "SUB":
+                result = (int)(stack[-1]) - (int)(stack[-2])
+                stack.pop(-1)
+                stack.pop(-1)
+                stack.append(result)
+            case "MUL":
+                result = (int)(stack[-1]) * (int)(stack[-2])
+                stack.pop(-1)
+                stack.pop(-1)
+                stack.append(result)
+            case "DIV":
+                result = (int)(stack[-1]) / (int)(stack[-2])
+                stack.pop(-1)
+                stack.pop(-1)
+                stack.append(result)
+            case "LOG":
+                print(stack[(int)(token[1])])
+            case "CHP":
+                print(chr((int)(stack[(int)(token[1])])), end="")
+            case "RGP":
+                print(chr((int)(registers[(int)(token[1])])), end="")
+            case "HLT":
+                print()
+                break
+            case "PSS":
+                print("", end="")
+            case other:
+                print("Unknown operation! : " + token[0])
+        if ip < len(text):
+            ip += 1
+        else:
+            return
+
+
+if __name__ == "__main__":
+    main()
+    input("Press enter to continue...")
