0d375f75a2b051058f88384e04663ad4f7644c4a
Raiyaxi-ZIaoi <82474669+Raiyaxi-Ziaoi@users.noreply.github.com>
2022-08-18 22:48:59 +0100

    Overhauled instruction set

--- README.md
+++ README.md
@@ -8,21 +8,7 @@
 <br>
 
 Keywords: <br/>
-PSH: Pushes a value to the stack; "PSH 3"<br/>
-ARG: Adds a register with default value '0'; "ARG"<br/>
-PLL: Pulls a value from a register and pushes it to the stack; "PLL 0"<br/>
-SET: Sets a register to a value from the stack; "SET 0 -1"<br/>
-MOV: Copies a value from one register to another and resets the first; "MOV 0 1"<br/>
-HLD: Makes a register hold a value specified; "HLD 0 1"<br/>
-POP: Deletes last value in the stack; "POP"<br/>
-ADD, SUB, MUL, DIV: Does arithmetic to the last two values of the stack and deletes them and pushes the result<br/>
-LOG: Logs value of member of stack given an indice; "LOG - 1"<br/>
-HLT: Halts the program; "HLT"<br/>
-PSS: Starts a one-line comment; "PSS 🗿"<br/>
-CHP: Prints a character given an indice of the value in question on the stack<br/>
-RGP: Prints a character given the register number<br/>
-MRG: Adds multiple registers; "MRG 10"<br/>
-JMP: Jumps to a specific line; "JMP 10"<br/>
-IF?: If Register is one it jumps to line specified; "IF 0 10"<br/>
-INC: Increments register by one; "INC 0"<br/>
-INP: Input to register; "INP 0"<br/>
+REG: Creates a new register; "REG"<br/>
+SET: Sets a register to a value; "SET, 0, "Hello World!"<br/>
+OUT: Prints register contents to output; "OUT, 0"<br/>
+EXT: Exits program; "EXT"<br/>
--- main.py
+++ main.py
@@ -9,67 +9,17 @@ def main():
     with open(input(), "r") as f:
         text = f.readlines()
     while True:
-        token = text[ip].split(' ')
+        token = text[ip].split(', ')
         ftoken = token[0].replace('\n', '')
         match ftoken:
-            case "PSH":
-                stack.append(token[1])
-            case "ARG":
-                registers.append(0)
-            case "MRG":
-                for x in range((int)(token[1])):
-                    registers.append(0)
-            case "PLL":
-                stack.append(registers[(int)(token[1])])
+            case "REG":
+                registers.append('0')
             case "SET":
-                registers[(int)(token[1])] = stack[(int)(token[2])]
-            case "MOV":
-                registers[(int)(token[1])] = registers[(int)(token[2])]
-                registers[(int)(token[1])] = 0
-            case "HLD":
-                registers[(int)(token[1])] = token[2]
-            case "POP":
-                stack.pop(-1)
-            case "ADD":
-                result = (int)(stack[-1]) + (int)(stack[-2])
-                stack.pop(-1)
-                stack.pop(-1)
-                stack.append(result)
-            case "SUB":
-                result = (int)(stack[-1]) - (int)(stack[-2])
-                stack.pop(-1)
-                stack.pop(-1)
-                stack.append(result)
-            case "MUL":
-                result = (int)(stack[-1]) * (int)(stack[-2])
-                stack.pop(-1)
-                stack.pop(-1)
-                stack.append(result)
-            case "DIV":
-                result = (int)(stack[-1]) / (int)(stack[-2])
-                stack.pop(-1)
-                stack.pop(-1)
-                stack.append(result)
-            case "LOG":
-                print(stack[(int)(token[1])])
-            case "CHP":
-                print(chr((int)(stack[(int)(token[1])])), end="")
-            case "RGP":
-                print(chr((int)(registers[(int)(token[1])])), end="")
-            case "HLT":
-                print()
+                registers[(int)(token[1])] = token[2].replace("\"", "")
+            case "OUT":
+                print(registers[(int)(token[1])])
+            case "EXT":
                 break
-            case "PSS":
-                print("", end="")
-            case "JMP":
-                ip = (int)(token[1]) - 2
-            case "IF?":
-                if registers[(int)(token[1])] == 1:
-                    ip = (int)(token[2]) - 2
-            case "INC":
-                registers[(int)(token[1])] += 1
-            case "INP":
-                registers[(int)(token[1])] = (int)(input())
             case other:
                 print("Unknown operation! : " + token[0])
         if ip < len(text):
