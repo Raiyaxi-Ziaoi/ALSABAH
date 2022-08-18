dd236f509d9a4eef0a090720d72997540679d66c
Raiyaxi-ZIaoi <82474669+Raiyaxi-Ziaoi@users.noreply.github.com>
2022-08-19 00:05:01 +0100

    Keywords and comments added

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ REG: Creates a new register; "REG"<br/>
 SET: Sets a register to a value; "SET, 0, "Hello World!"<br/>
 OUT: Prints register contents to output; "OUT, 0"<br/>
 EXT: Exits program; "EXT"<br/>
+PSH: Push value to stack; "PSH, 5"<br/>
+ADD: Adds the last two values in the stack together; "ADD"<br/>
+STS: Sets register value from last value in the stack; "STS, 0"<br/>
+JMP: Jumps to line specified; "JMP, 0"<br/>
--- main.py
+++ main.py
@@ -1,24 +1,58 @@
 
-def main():
-    text = []
-    stack = []
-    registers = []
-    sp = 0
-    ip = 0
+def main():  # Main function
+    text = []  # File contents
+    stack = []  # Stack
+    registers = []  # Registers
+    # sp = 0
+    ip = 0  # Instruction pointer
     print("Please enter a filename: ")
-    with open(input(), "r") as f:
-        text = f.readlines()
-    while True:
-        token = text[ip].split(', ')
-        ftoken = token[0].replace('\n', '')
-        match ftoken:
-            case "REG":
+    with open(input(), "r") as f:   # Open file
+        text = f.readlines()        # Read contents to 'text' as a list
+    while True:  # Loop
+        token = text[ip].split(', ')  # Tokens split by ", "
+        ftoken = token[0].replace('\n', '')  # Clean first token
+        match ftoken:  # Match cleaned token
+            case "REG":  # Create empty register
                 registers.append('0')
-            case "SET":
+            case "SET":  # Set register to value
                 registers[(int)(token[1])] = token[2].replace("\"", "")
-            case "OUT":
+            case "OUT":  # Output contentes of register
                 print(registers[(int)(token[1])])
-            case "EXT":
+            case "PSH":  # Push value to stack
+                stack.append(token[1])
+            case "ADD":  # Adds last two stack values
+                result = (int)(stack[-1]) + (int)(stack[-2])
+                stack.pop(-1)
+                stack.pop(-1)
+                stack.append(result)
+            case "SUB":  # Subtracts last two stack values
+                result = (int)(stack[-1]) - (int)(stack[-2])
+                stack.pop(-1)
+                stack.pop(-1)
+                stack.append(result)
+            case "MUL":  # Multiplies last two stack values
+                result = (int)(stack[-1]) * (int)(stack[-2])
+                stack.pop(-1)
+                stack.pop(-1)
+                stack.append(result)
+            case "DIV":  # Divides last two stack values
+                result = (int)(stack[-1]) / (int)(stack[-2])
+                stack.pop(-1)
+                stack.pop(-1)
+                stack.append(result)
+            case "MOD":  # Modulo operation on last two stack values
+                result = (int)(stack[-1]) % (int)(stack[-2])
+                stack.pop(-1)
+                stack.pop(-1)
+                stack.append(result)
+            case "STS":  # Set register from stack
+                registers[(int)(token[1])] = stack[-1]
+                stack.pop(-1)
+            case "JMP": # Jumps to line
+                ip = (int)(token[1]) - 1
+            case "INP": # Takes input and stores in register
+                registers[(int)(token[1])] = input()
+            case "EXT":  # Exit
                 break
             case other:
                 print("Unknown operation! : " + token[0])
@@ -30,4 +64,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    input("Press enter to continue...")
+    input("Press enter to continue...")  # Catch to stop exit
